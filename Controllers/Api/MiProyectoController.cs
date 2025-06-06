b88b696b7d61a12a5bcca241aa301643bba4df0a
Roger-512 <rogelio.sanchezbarron.gen23@cbtis105.edu.mx>
2025-06-06 08:51:19 -0600

    ce fini

--- Controllers/Api/MiProyectoController.cs
+++ Controllers/Api/MiProyectoController.cs
@@ -1,17 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 
 [ApiController]
 [Route("mi-proyecto")]
-public class MiProyectoController : ControllerBase {
-    
+public class MiProyectoController : ControllerBase
+{
+   
+
     [HttpGet("integrantes")]
-    public IActionResult Integrantes(){
+    public IActionResult Integrantes()
+    {
         var proyecto = new MiProyecto
-            {
-                NombreIntegrante1 = "Angelina Martínez García",
-                NombreIntegrante2 = "Rogelio Sánchez Barrón "
-            };
+        {
+            NombreIntegrante1 = "Angelina Martínez García",
+            NombreIntegrante2 = "Rogelio Sánchez Barrón "
+        };
 
         return Ok(proyecto);
     }
+
+    [HttpGet("presentacion")]
+    public IActionResult Presentacion()
+    {
+        MongoClient client = new MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Escuela_Angelina_Rogelio");
+        var collection = db.GetCollection<Equipo>("Equipo");
+
+        var lista = collection.Find(FilterDefinition<Equipo>.Empty).ToList();
+
+        return Ok(lista);
+        
+    }
 }
+
